780b8f2f685be82f87e8305bbd029ea0ad9361b5
mhbagheri-99 <mhbagheri3@gmail.com>
2024-06-05 17:50:42 +0200

    add new post function

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from . import forms 
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
@@ -12,4 +13,13 @@ def posts_page(request, slug):
 
 @login_required(login_url="/users/login/")
 def posts_new(request):
-    return render(request, 'posts/posts_new.html')
+    if request.method == 'POST': 
+        form = forms.CreatePost(request.POST, request.FILES) 
+        if form.is_valid():
+            newpost = form.save(commit=False) 
+            newpost.author = request.user 
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/posts_new.html', { 'form': form })
